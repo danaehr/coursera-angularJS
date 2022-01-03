2a5dbb0e44a83e8f6e074166c3b142d1d3e35fd7
danaehr <80633680+danaehr@users.noreply.github.com>
2022-01-03 12:17:56 +0100

    download source and developement solution 2

---
+++ module2_solution/app.js
@@ -0,0 +1,60 @@
+(function (){
+'use strict';
+
+angular.module('ShoppingListCheckOff',[])
+.controller('ToBuyController', BuyController)
+.controller('AlreadyBoughtController', BoughtController)
+.service('ShoppingListCheckOffService', CheckOffService);
+
+BuyController.$inject = ['ShoppingListCheckOffService'];
+function BuyController(ShoppingListCheckOffService){
+	var buy = this;
+
+	//buy.itemName,buy.itemQuantity="";
+
+	buy.items=ShoppingListCheckOffService.getShoppingList();
+
+	buy.buyItem = function(itemIndex){
+		ShoppingListCheckOffService.buyItem(itemIndex);
+	};
+}
+
+BoughtController.$inject = ['ShoppingListCheckOffService'];
+function BoughtController(ShoppingListCheckOffService){
+	var bought = this;
+
+	bought.items=ShoppingListCheckOffService.getBoughtItems();
+}
+
+function CheckOffService(){
+	var service = this;
+
+	var shoppingList = [
+		{name: "table", quantity: "1"},
+		{name: "apples", quantity: "10"},
+		{name: "snacks", quantity: "23"},
+		{name: "chairs", quantity: "4"},
+		{name: "cookies", quantity: "50"}
+		];
+	var boughtItems = [];
+
+	service.getShoppingList = function(){
+		return shoppingList;
+	};
+
+	service.getBoughtItems = function(){
+		return boughtItems;
+	};
+
+	service.buyItem = function(itemIndex){
+		//remove from shoppingList
+		var item = shoppingList.splice(itemIndex,1)[0];
+		
+		//add to boughtItems
+		boughtItems.push(item);
+
+	};
+
+}
+
+})();
