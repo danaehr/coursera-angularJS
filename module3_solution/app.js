97b0843695000232da375f3620813f4a4f9b7a8f
danaehr <80633680+danaehr@users.noreply.github.com>
2022-01-05 08:55:56 +0100

    module3_solution

---
+++ module3_solution/app.js
@@ -0,0 +1,106 @@
+(function (){
+'use strict';
+
+angular.module('NarrowItDownApp',[])
+.controller('NarrowItDownController', NarrowItDownControllerFct)
+.service('MenuSearchService', MenuSearchServiceFct)
+.constant('ApiBasePath',"https://davids-restaurant.herokuapp.com")
+.directive('foundItems',foundItemsFct);
+
+
+NarrowItDownControllerFct.$inject = ['MenuSearchService'];
+function NarrowItDownControllerFct(MenuSearchService){
+	var nid = this;
+
+	nid.searchTerm="";
+	nid.found=[];
+
+	nid.getMatchedMenuItems=function(){
+		if(nid.searchTerm != ""){
+			MenuSearchService.getMatchedMenuItems(nid.searchTerm).then(
+			function(result){
+				nid.found=result;
+			});
+		}
+		else{
+			nid.found = [];
+		}
+		
+	};
+
+	nid.removeItem=function(itemIndex){
+		//var objectRemoved = 
+    	nid.found.splice(itemIndex,1);
+
+    	//console.log("removed item: ");
+    	//console.log(objectRemoved);
+	};
+}
+
+MenuSearchServiceFct.$inject = ['$q','$http','ApiBasePath'];
+function MenuSearchServiceFct($q,$http,ApiBasePath){
+
+	var service = this;
+
+	service.getMatchedMenuItems=function(searchTerm){	
+		var deferred = $q.defer();
+		var result = [];
+
+		$http(
+			{
+				method: "GET",
+				url:(ApiBasePath + "/menu_items.json"),
+			}
+			).then( 
+				function(result){
+					var foundItems = [];
+					var allItems = result.data.menu_items;
+
+					for(var i = 0; i < allItems.length; i++){
+						if(allItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase())>=0){
+							foundItems.push(allItems[i]);
+						}
+					}
+					deferred.resolve(foundItems);
+
+				}
+			).catch(
+				function(error){
+					deferred.reject(error);
+				}
+			);
+
+			return deferred.promise;
+	};
+
+}
+
+function foundItemsFct(){
+	var ddo = {
+		templateUrl: 'templFoundItems.html',
+		scope: {
+			listFoundItems: '<',//'<foundItemsList',
+			onRemove: '&'
+		},
+		controller: foundItemsDirectiveController,
+		controllerAs: 'fidc',
+		bindToController: true
+	};
+
+	return ddo;
+}
+
+function foundItemsDirectiveController(){
+	var fidc = this;
+
+	fidc.nothingFoundCheck = function(){
+		if(fidc.listFoundItems.length == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	};
+}
+
+})();
